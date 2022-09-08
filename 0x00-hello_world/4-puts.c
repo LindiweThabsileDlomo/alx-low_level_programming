0f138fe237f536b0f94f173698766d651ab3d01d
LindiweThabsileDlomo <lindiwethaby@hotmail.com>
2022-09-08 15:22:47 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#includ <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+* this program prints "Programing is like building a multilingual puzzle"
+* return: 0
+*/
+int main(void)
+{
+       puts("\"Programing is like building a multilingual puzzle");
+       return 0;
+
+}
